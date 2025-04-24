4ac5075e5c26a12e1dc5958bcb8eaaeddc679d8d
Szymon Drdzeń <szymond_1@wp.pl>
2025-04-24 14:46:44 +0200

    init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Apkalikacja2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.gMapControl11 = new GMap.NET.WindowsForms.GMapControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(156, 7);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(184, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 92);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(334, 186);
+            this.listBox1.TabIndex = 1;
+            // 
+            // gMapControl11
+            // 
+            this.gMapControl11.Bearing = 0F;
+            this.gMapControl11.CanDragMap = true;
+            this.gMapControl11.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl11.GrayScaleMode = false;
+            this.gMapControl11.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl11.LevelsKeepInMemory = 5;
+            this.gMapControl11.Location = new System.Drawing.Point(372, 23);
+            this.gMapControl11.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gMapControl11.MarkersEnabled = true;
+            this.gMapControl11.MaxZoom = 2;
+            this.gMapControl11.MinZoom = 2;
+            this.gMapControl11.MouseWheelZoomEnabled = true;
+            this.gMapControl11.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl11.Name = "gMapControl11";
+            this.gMapControl11.NegativeMode = false;
+            this.gMapControl11.PolygonsEnabled = true;
+            this.gMapControl11.RetryLoadTile = 0;
+            this.gMapControl11.RoutesEnabled = true;
+            this.gMapControl11.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl11.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl11.ShowTileGridLines = false;
+            this.gMapControl11.Size = new System.Drawing.Size(412, 524);
+            this.gMapControl11.TabIndex = 2;
+            this.gMapControl11.Zoom = 0D;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 53);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 19);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Plik 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(6, 311);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(334, 238);
+            this.listBox2.TabIndex = 5;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(-2, 9);
+            this.label11.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(154, 13);
+            this.label11.TabIndex = 6;
+            this.label11.Text = "Podaj nr MMSI swojego statku:";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(6, 292);
+            this.label12.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(128, 13);
+            this.label12.TabIndex = 7;
+            this.label12.Text = "Zdekodowane Informacje";
+            this.label12.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 73);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Dekodowane komunikaty";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(126, 53);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 19);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Plik 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 36);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(239, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Wybierz plik z którego dane będziesz dekodował";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 552);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gMapControl11);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "MapAIS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private GMap.NET.WindowsForms.GMapControl gMapControl11;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,260 @@
+﻿using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.WindowsForms;
+using GMap.NET;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GMap.NET.WindowsPresentation;
+using GMapMarker = GMap.NET.WindowsForms.GMapMarker;
+
+namespace Apkalikacja2
+{
+    public partial class Form1 : Form
+    {
+        private long my_mmsi;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void odczytywanie_mmsi()
+        {
+            // Odczytanie MMSI z textboxa  
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Pole MMSI nie może być puste.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            try
+            {
+                my_mmsi = Convert.ToInt64(textBox1.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Wprowadź poprawny numer MMSI.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        public void rysowanie(int mmsi, double lon, double lat)
+        {
+            gMapControl11.Position = new PointLatLng(lat, lon);
+            gMapControl11.Zoom = 12;
+            gMapControl11.MinZoom = 4;
+            gMapControl11.MaxZoom = 50;
+            GMapOverlay markers = new GMapOverlay("markers");
+            if (mmsi == my_mmsi)
+            {
+                GMapMarker marker =
+                new GMarkerGoogle(
+                    new PointLatLng(lat, lon),
+                    GMarkerGoogleType.blue_dot);
+                markers.Markers.Add(marker);
+            }
+            else
+            {
+                GMapMarker marker =
+                new GMarkerGoogle(
+                    new PointLatLng(lat, lon),
+                    GMarkerGoogleType.red_dot);
+                markers.Markers.Add(marker);
+            }
+            gMapControl11.Overlays.Add(markers);
+        }
+
+        public void dekodowanie(string payload, string checksum)
+        {
+            //Dekodowanie
+            string binary = "";
+            foreach (char c in payload)
+            {
+                int i = c - 48;
+                if (i > 40)
+                {
+                    i -= 8;
+                }
+                binary += Convert.ToString(i, 2).PadLeft(6, '0');
+            }
+
+            //Typ komunikatu
+            int msg_type = Convert.ToInt32(binary.Substring(0, 6), 2);
+            listBox2.Items.Add("Message type: " + msg_type);
+
+            switch (msg_type)
+            {
+                case 1:
+                    komunikat_1(binary);
+                    break;
+                case 3:
+                    komunikat_1(binary);
+                    break;
+                case 4:
+                    komunikat_4(binary);
+                    break;
+                case 5:
+                    break;
+                default:
+                    listBox2.Items.Add("Zły typ komunikatu");
+                    break;
+            }
+        }
+
+        //Wpisywanie danych do listobxa
+        private void komunikat_1(string binary)
+        {
+            int mmsi = Convert.ToInt32(binary.Substring(8, 30), 2);
+            int status = Convert.ToInt32(binary.Substring(38, 2), 2);
+            string[] status_desc = { "Under way using engine", "At anchor", "Not under command", "Restricted manoeuverability" };
+            int rot = Convert.ToInt32(binary.Substring(42, 8), 2);
+            double cog = Convert.ToDouble(Convert.ToInt64(binary.Substring(116, 12), 2)) / 10;
+            double sog = Convert.ToDouble(Convert.ToInt64(binary.Substring(50, 10), 2)) / 10;
+            double lon = Convert.ToDouble(Convert.ToInt64(binary.Substring(61, 28), 2)) / 600000;
+            double lat = Convert.ToDouble(Convert.ToInt64(binary.Substring(89, 27), 2)) / 600000;
+            int true_heading = Convert.ToInt32(binary.Substring(128, 9), 2);
+            rysowanie(mmsi, lon, lat);
+            listBox2.Items.Add("MMSI: " + mmsi);
+            listBox2.Items.Add("Status: " + status);
+            listBox2.Items.Add("Status description: " + status_desc[status]);
+            listBox2.Items.Add("Lon: " + lon);
+            listBox2.Items.Add("Lat: " + lat);
+            listBox2.Items.Add("COG: " + cog);
+            listBox2.Items.Add("SOG: " + sog);
+            listBox2.Items.Add("ROT: " + rot);
+            listBox2.Items.Add("True heading: " + true_heading);
+        }
+
+        private void komunikat_4(string binary)
+        {
+            int mmsi = Convert.ToInt32(binary.Substring(8, 30), 2);
+            int rok = Convert.ToInt32(binary.Substring(38, 14), 2);
+            int miesiac = Convert.ToInt32(binary.Substring(52, 4), 2);
+            int dzien = Convert.ToInt32(binary.Substring(56, 5), 2);
+            int godzina = Convert.ToInt32(binary.Substring(61, 5), 2);
+            int minuta = Convert.ToInt32(binary.Substring(66, 6), 2);
+            int secunda = Convert.ToInt32(binary.Substring(72, 6), 2);
+            double lon = Convert.ToDouble(Convert.ToInt64(binary.Substring(79, 28), 2)) / 600000;
+            double lat = Convert.ToDouble(Convert.ToInt64(binary.Substring(107, 27), 2)) / 600000;
+
+            rysowanie(mmsi, lon, lat);
+
+            listBox2.Items.Add("MMSI: " + mmsi);
+            listBox2.Items.Add("Data: " + rok + "-" + miesiac.ToString("D2") + "-" + dzien.ToString("D2"));
+            listBox2.Items.Add("Czas: " + godzina.ToString("D2") + ":" + minuta.ToString("D2") + ":" + secunda.ToString("D2"));
+            listBox2.Items.Add("Lon: " + lon);
+            listBox2.Items.Add("Lat: " + lat);
+        }
+
+
+        //Pobieranie danych z pliku 1
+        public void Dane_Plik1()
+        {
+            listBox1.Items.Clear();
+            using (StreamReader reader = new StreamReader("Resources//kom_sensor_ais.txt"))
+            {
+                string line = reader.ReadLine();
+                string payload = "";
+                string checksum = "";
+                while (line != null)
+                {
+                    if (line.Contains("AIVDM"))
+                    {
+                        string[] words = line.Split(',');
+                        if (words[1] == "1")
+                        {
+                            payload = words[5];
+                            checksum = words[6];
+                            dekodowanie(payload, checksum);
+                            listBox1.Items.Add(line);
+                        }
+                        else
+                        {
+                            payload += words[5];
+                            listBox1.Items.Add(line);
+                        }
+                    }
+                    line = reader.ReadLine();
+                }
+            }
+        }
+
+        //Pobieranie danych z pliku 2
+        public void Dane_Plik2()
+        {
+            listBox1.Items.Clear();
+            using (StreamReader reader = new StreamReader("Resources//kom_sensor_ais2.txt"))
+            {
+                string line = reader.ReadLine();
+                string payload = "";
+                string checksum = "";
+                while (line != null)
+                {
+                    if (line.Contains("AIVDM"))
+                    {
+                        string[] words = line.Split(',');
+                        if (words[1] == "1")
+                        {
+                            payload = words[5];
+                            checksum = words[6];
+                            dekodowanie(payload, checksum);
+                            listBox1.Items.Add(line);
+                        }
+                        else
+                        {
+                            payload += words[5];
+                            listBox1.Items.Add(line);
+                        }
+                    }
+                    line = reader.ReadLine();
+                }
+            }
+        }
+        //Przycisk do wyswietlania danych 1
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            odczytywanie_mmsi();
+            Dane_Plik1();
+            gMapControl11.MapProvider = GoogleMapProvider.Instance;
+            GMaps.Instance.Mode = AccessMode.ServerOnly;
+        }
+
+        //Przycisk do wyswietlania danych 2
+        private void button2_Click(object sender, EventArgs e)
+        {
+            odczytywanie_mmsi();
+            Dane_Plik2();
+            gMapControl11.MapProvider = GoogleMapProvider.Instance;
+            GMaps.Instance.Mode = AccessMode.ServerOnly;
+        }
+
+
+
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
